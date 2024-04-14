5bdb56c433def9312280e8818c7136329a54c9b2
Santi-2404 <santkrdzv24@gmail.com>
2024-04-14 08:14:59 -0500

    Agregar archivos de proyecto.

---
+++ ciudades/Form1.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        RadioButton3 = New RadioButton()
+        GroupBox1 = New GroupBox()
+        PictureBox1 = New PictureBox()
+        RichTextBox1 = New RichTextBox()
+        AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
+        GroupBox1.SuspendLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(AxWindowsMediaPlayer1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(6, 22)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(85, 19)
+        RadioButton1.TabIndex = 0
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Tamaulipas"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(109, 22)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(65, 19)
+        RadioButton2.TabIndex = 1
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Oaxaca"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(195, 22)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(66, 19)
+        RadioButton3.TabIndex = 2
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "Chiapas"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(RadioButton1)
+        GroupBox1.Controls.Add(RadioButton3)
+        GroupBox1.Controls.Add(RadioButton2)
+        GroupBox1.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        GroupBox1.Location = New Point(12, 12)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(300, 65)
+        GroupBox1.TabIndex = 3
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "ESTADOS"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BorderStyle = BorderStyle.Fixed3D
+        PictureBox1.Location = New Point(358, 12)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(347, 202)
+        PictureBox1.SizeMode = PictureBoxSizeMode.AutoSize
+        PictureBox1.TabIndex = 4
+        PictureBox1.TabStop = False
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.BackColor = Color.FromArgb(CByte(0), CByte(192), CByte(192))
+        RichTextBox1.Location = New Point(12, 83)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.Size = New Size(300, 321)
+        RichTextBox1.TabIndex = 5
+        RichTextBox1.Text = ""
+        ' 
+        ' AxWindowsMediaPlayer1
+        ' 
+        AxWindowsMediaPlayer1.Enabled = True
+        AxWindowsMediaPlayer1.Location = New Point(358, 236)
+        AxWindowsMediaPlayer1.Name = "AxWindowsMediaPlayer1"
+        AxWindowsMediaPlayer1.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer1.OcxState"), AxHost.State)
+        AxWindowsMediaPlayer1.Size = New Size(347, 273)
+        AxWindowsMediaPlayer1.TabIndex = 6
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        ClientSize = New Size(717, 521)
+        Controls.Add(AxWindowsMediaPlayer1)
+        Controls.Add(RichTextBox1)
+        Controls.Add(PictureBox1)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(AxWindowsMediaPlayer1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
+
+End Class
---
+++ ciudades/Form1.vb
@@ -0,0 +1,68 @@
+﻿Public Class Form1
+    Private Sub RadioButton1_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles RadioButton1.CheckedChanged, RadioButton2.CheckedChanged
+
+        If RadioButton1.Checked = True Then
+            Try
+                PictureBox1.Image = Image.FromFile("Tamaulipas.png")
+            Catch ex As Exception
+            End Try
+            Try
+                RichTextBox1.LoadFile("Tamaulipas.rtf")
+
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+
+            End Try
+            AxWindowsMediaPlayer1.URL = "TAMAULIPAS.mp4"
+            AxWindowsMediaPlayer1.Ctlcontrols.stop()
+        ElseIf RadioButton2.Checked = True Then
+            Try
+                PictureBox1.Image = Image.FromFile("Oaxaca.jpg")
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+
+            End Try
+            Try
+                RichTextBox1.LoadFile("OAXACA.rtf")
+
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+
+            End Try
+            AxWindowsMediaPlayer1.URL = "OAXACA.mp4"
+            AxWindowsMediaPlayer1.Ctlcontrols.stop()
+        ElseIf RadioButton3.Checked = True Then
+            Try
+                PictureBox1.Image = Image.FromFile("chiapas.jpg")
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+
+            End Try
+            Try
+                RichTextBox1.LoadFile("chiapas.rtf")
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+
+            End Try
+            AxWindowsMediaPlayer1.URL = "CHIAPAS.mp4"
+        End If
+
+    End Sub
+    Private Sub RadioButton3_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles RadioButton3.CheckedChanged
+
+        Try
+            PictureBox1.Image = Image.FromFile("chiapas.jpg")
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+        Try
+            RichTextBox1.LoadFile("chiapas.rtf")
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+        AxWindowsMediaPlayer1.URL = "CHIAPAS.mp4"
+        AxWindowsMediaPlayer1.Ctlcontrols.stop()
+    End Sub
+
+
+End Class
